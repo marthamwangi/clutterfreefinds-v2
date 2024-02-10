c2406d06632f15ce2e2a3d327e0f289cc3539891
marthamwangi <martyturing@gmail.com>
2024-02-10 08:52:29 +0300

    chore: (component): cleanup app component and change from defaults

--- apps/website/src/app/app.component.ts
+++ apps/website/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'clutterfreefinds-v2-root',
+  imports: [RouterModule],
+  selector: 'cff-v2-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
