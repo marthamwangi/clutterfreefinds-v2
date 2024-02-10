2c58c3e255794d5e414d7f988f3501c1a098a3aa
marthamwangi <martyturing@gmail.com>
2024-02-10 08:51:35 +0300

    chore: (route) load outlets

--- apps/website/src/app/app.routes.ts
+++ apps/website/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/home-page/home-page.component').then(
+        (component) => component.HomePageComponent
+      ),
+    pathMatch: 'full',
+  },
+];
