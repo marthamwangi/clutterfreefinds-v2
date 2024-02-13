19ca238ba4e92d76035c12859a61efe794db3a16
marthamwangi <martyturing@gmail.com>
2024-02-13 13:14:56 +0300

    chore(component): the process section

---
+++ apps/website/src/app/components/home-page/sections/the-process/the-process.component.ts
@@ -0,0 +1,44 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'cff-v2-the-process',
+  standalone: true,
+  imports: [TranslateModule, NgFor],
+  templateUrl: './the-process.component.html',
+  styleUrl: './the-process.component.scss',
+})
+export class TheProcessComponent {
+  public processes: Array<{
+    number: string;
+    title: string;
+    description: string;
+    image: string;
+  }> = [
+    {
+      number: '1',
+      title: 'HOME_PAGE.THE_PROCESS.PROCESS.ITEM_1.TITLE',
+      description: 'HOME_PAGE.THE_PROCESS.PROCESS.ITEM_1.DESCRIPTION',
+      image: 'consult.png',
+    },
+    {
+      number: '2',
+      title: 'HOME_PAGE.THE_PROCESS.PROCESS.ITEM_2.TITLE',
+      description: 'HOME_PAGE.THE_PROCESS.PROCESS.ITEM_2.DESCRIPTION',
+      image: 'estimate.png',
+    },
+    {
+      number: '3',
+      title: 'HOME_PAGE.THE_PROCESS.PROCESS.ITEM_3.TITLE',
+      description: 'HOME_PAGE.THE_PROCESS.PROCESS.ITEM_3.DESCRIPTION',
+      image: 'schedule.png',
+    },
+    {
+      number: '4',
+      title: 'HOME_PAGE.THE_PROCESS.PROCESS.ITEM_4.TITLE',
+      description: 'HOME_PAGE.THE_PROCESS.PROCESS.ITEM_4.DESCRIPTION',
+      image: 'transform.png',
+    },
+  ];
+}
