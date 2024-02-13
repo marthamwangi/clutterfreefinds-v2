e424d5bcf04d63dcb628882afd0b44a64211a3ae
marthamwangi <martyturing@gmail.com>
2024-02-13 13:32:17 +0300

    chore(component): our community section

---
+++ apps/website/src/app/components/home-page/sections/our-community/our-community.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'cff-v2-our-community',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './our-community.component.html',
+  styleUrl: './our-community.component.scss',
+})
+export class OurCommunityComponent {}
