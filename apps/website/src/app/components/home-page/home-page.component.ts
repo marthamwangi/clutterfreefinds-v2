eaca3cab051d1602d255c822eacc7e5c81743159
marthamwangi <martyturing@gmail.com>
2024-02-15 20:10:16 +0300

    chore:(components) add components to the home page

--- apps/website/src/app/components/home-page/home-page.component.ts
+++ apps/website/src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { HeroComponent } from './sections/hero/hero.component';
+import { WhyCffComponent } from './sections/why-cff/why-cff.component';
+import { WhatWeDoComponent } from './sections/what-we-do/what-we-do.component';
+import { TheProcessComponent } from './sections/the-process/the-process.component';
+import { OurCommunityComponent } from './sections/our-community/our-community.component';
+import { HowWeDoItComponent } from './sections/how-we-do-it/how-we-do-it.component';
+import { FaqSectionComponent } from '../faq-section/faq-section.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'cff-v2-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    WhatWeDoComponent,
+    WhyCffComponent,
+    TheProcessComponent,
+    OurCommunityComponent,
+    HowWeDoItComponent,
+    FaqSectionComponent,
+    FooterComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
