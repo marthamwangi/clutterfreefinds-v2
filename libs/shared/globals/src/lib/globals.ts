84df00235bb1ffad02f94ee6fdb6969dfccd8bb5
marthamwangi <martyturing@gmail.com>
2024-02-20 12:54:15 +0300

    new: shared global for variables

---
+++ libs/shared/globals/src/lib/globals.ts
@@ -0,0 +1,12 @@
+export const CFF_SOCIAL_ACCOUNTS = {
+  FACEBOOK: 'https://www.facebook.com/clutter.free.finds.fb/',
+  INSTAGRAM: 'https://www.instagram.com/clutterfreefinds/',
+  TIKTOK: 'https://www.tiktok.com/@clutterfreefinds',
+};
+export const GOOGLE_BUSINESS_PROFILE =
+  'https://maps.app.goo.gl/K6VBPGG1ghWex2TX9';
+export const CFF_WHATS_APP_LINK = 'wa.me/+254706347399';
+export const EMAIL_LIST__DEPLOYMENT_ID =
+  'AKfycbx8TpK6N3kKA671lEc6dNovzE9xSa46a_l0-6MfLj4Gd8UDbIbjCzJY9XDhBFLv6Oh5kA';
+export const WEB_APP_EMAIL =
+  'https://script.google.com/macros/s/AKfycbx8TpK6N3kKA671lEc6dNovzE9xSa46a_l0-6MfLj4Gd8UDbIbjCzJY9XDhBFLv6Oh5kA/exec';
