e9fc945ee0b04e5483b21a090f084e06605836ad
marthamwangi <martyturing@gmail.com>
2024-02-27 17:16:36 +0300

    fix: error on ssr rendering content in application by separating browser and server module

--- apps/website/mfe/src/app/app.server.module.ts
+++ apps/website/mfe/src/app/app.server.module.ts
@@ -3,12 +3,10 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { I18nServerModuleModule } from './i18n/i18n-server-module.module';
 
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
+  imports: [AppModule, ServerModule, I18nServerModuleModule],
   bootstrap: [AppComponent],
 })
 export class AppServerModule {}
