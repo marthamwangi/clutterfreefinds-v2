658bed6a568a73d358428ea5a3b1e589e50cab36
marthamwangi <martyturing@gmail.com>
2024-02-28 09:10:08 +0300

    refactor: delete unused globals and copy to vars

--- libs/shared/globals/src/lib/variables/vars.ts
+++ libs/shared/globals/src/lib/variables/vars.ts
@@ -9,3 +9,8 @@ export const CFF_WHATS_APP_LINK = 'wa.me/+254706347399';
 export const WEB_APP_EMAIL_SUBSCRIBER_LIST =
   'https://script.google.com/macros/s/AKfycbxah1DJS8XoUrrpbpkT7HotWt5q0DuM_493OErMe_GR2CwLrCyQwcUmyQHU7D60ttwjEA/exec';
 export const DEFAULT_APP_LANGUAGE = 'en';
+export const SUPPORTED_LANGUAGES: any = {
+  en: 'en',
+  sw: 'sw',
+};
+export const EMAIL = 'clutterfreefinds@gmail.com';
