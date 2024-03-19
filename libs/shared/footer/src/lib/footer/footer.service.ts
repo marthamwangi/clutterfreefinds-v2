3c333f484c484e18ba6f948b84ee32c358df1dc2
marthamwangi <martyturing@gmail.com>
2024-03-19 15:47:05 +0300

    chore: cleanup and add more stylings

--- libs/shared/footer/src/lib/footer/footer.service.ts
+++ libs/shared/footer/src/lib/footer/footer.service.ts
@@ -6,13 +6,8 @@ import { Injectable } from '@angular/core';
 })
 export class FooterService {
   constructor(private _httpClient: HttpClient) {}
-  private headers = new Headers({
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-  });
-  private options = { headers: this.headers };
 
-  newsLetterService(url: string, body: any, params: object = this.options) {
+  newsLetterService(url: string, body: any, params: object) {
     return this._httpClient.post(url, body, {
       params: {
         ...params,
