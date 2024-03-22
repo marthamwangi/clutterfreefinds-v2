cc01d4bfd48f3f28449d78c47fd5493754d77ee4
marthamwangi <martyturing@gmail.com>
2024-03-22 15:37:44 +0300

    chore:initialize instant quote component

--- apps/website/mfe/src/app/components/home-page/home-page.component.ts
+++ apps/website/mfe/src/app/components/home-page/home-page.component.ts
@@ -5,7 +5,7 @@ import { WhatWeDoComponent } from './sections/what-we-do/what-we-do.component';
 import { TheProcessComponent } from './sections/the-process/the-process.component';
 import { OurCommunityComponent } from './sections/our-community/our-community.component';
 import { HowWeDoItComponent } from './sections/how-we-do-it/how-we-do-it.component';
-import { FaqSectionComponent } from '../faq-section/faq-section.component';
+import { FaqSectionComponent } from './sections/faq-section/faq-section.component';
 import { FooterComponent } from '@clutterfreefinds-v2/footer';
 import { HowItWorksComponent } from '../how-it-works/how-it-works.component';
 @Component({
