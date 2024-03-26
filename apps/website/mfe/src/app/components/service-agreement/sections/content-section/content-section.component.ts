6255eb54a39616732536dabcf66bba6fc96506e4
marthamwangi <martyturing@gmail.com>
2024-03-26 10:03:36 +0300

    new:component for service agreement

---
+++ apps/website/mfe/src/app/components/service-agreement/sections/content-section/content-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sa-content-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './content-section.component.html',
+  styleUrls: ['./content-section.component.scss'],
+})
+export class ContentSectionComponent {}
---
+++ apps/website/mfe/src/app/components/service-agreement/service-agreement.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'sa-service-agreement',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './service-agreement.component.html',
+  styleUrls: ['./service-agreement.component.scss'],
+})
+export class ServiceAgreementComponent {}
