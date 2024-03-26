b23c16a33ef1883cf532f992cf7aac7f545f6f64
marthamwangi <martyturing@gmail.com>
2024-03-26 10:36:50 +0300

    new:component about us

---
+++ apps/website/mfe/src/app/components/about-us/about-us.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'wc-about-us',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './about-us.component.html',
+  styleUrls: ['./about-us.component.scss'],
+})
+export class AboutUsComponent {}
