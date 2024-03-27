56a476cc7261c3a4de211e213372062dc0b3dede
marthamwangi <martyturing@gmail.com>
2024-03-27 15:15:12 +0300

    wip: header styling

--- libs/shared/header/src/lib/header/header.component.ts
+++ libs/shared/header/src/lib/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageComponent } from '@clutterfreefinds-v2/language';
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'cff-header',
   standalone: true,
-  imports: [TranslateModule, LanguageComponent],
+  imports: [TranslateModule, LanguageComponent, NgFor],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
