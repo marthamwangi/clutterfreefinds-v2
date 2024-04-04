f56afcd090feadcc21676d42552d8ebd4eb19f04
marthamwangi <martyturing@gmail.com>
2024-04-04 13:53:56 +0300

    create accordion for selecting products/materials

---
+++ apps/website/mfe/src/app/pages/instant-quote/client/sections/quote-product/quote-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'iq-quote-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './quote-product.component.html',
+  styleUrls: ['./quote-product.component.scss'],
+})
+export class QuoteProductComponent {}
