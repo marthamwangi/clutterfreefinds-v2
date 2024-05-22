204c9a68a55fd67c29d9cd803ccd5bdb7f0be403
marthamwangi <martyturing@gmail.com>
2024-05-22 04:43:42 +0300

    chore: service agreement page

--- apps/website/mfe/src/app/pages/service-agreement/service-agreement.component.ts
+++ apps/website/mfe/src/app/pages/service-agreement/service-agreement.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HowItWorksComponent } from '../../components/how-it-works/how-it-works.component';
 import { ContentSectionComponent } from './sections/content-section/content-section.component';
 import { HeroSectionComponent } from './sections/hero-section/hero-section.component';
 
 @Component({
   selector: 'sa-service-agreement',
   standalone: true,
-  imports: [HowItWorksComponent, ContentSectionComponent, HeroSectionComponent],
+  imports: [ContentSectionComponent, HeroSectionComponent],
   templateUrl: './service-agreement.component.html',
 })
 export class ServiceAgreementComponent {}
