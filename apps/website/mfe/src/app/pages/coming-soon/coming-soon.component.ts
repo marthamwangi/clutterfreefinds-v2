f81713bc39684344f3d9ae00a809e3b23384f0aa
marthamwangi <martyturing@gmail.com>
2024-05-23 15:24:26 +0300

    chore: add coming soon page

--- apps/website/mfe/src/app/pages/coming-soon/coming-soon.component.ts
+++ apps/website/mfe/src/app/pages/coming-soon/coming-soon.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { CFF_SOCIAL_ACCOUNTS } from '@clutterfreefinds-v2/globals';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'cs-coming-soon',
   standalone: true,
-  imports: [CommonModule],
+  imports: [RouterLink],
   templateUrl: './coming-soon.component.html',
-  styleUrls: ['./coming-soon.component.scss'],
 })
 export class ComingSoonComponent {
   CFF_SOCIAL_ACCOUNTS = CFF_SOCIAL_ACCOUNTS;
