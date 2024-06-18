90c3f8f61d0fa1a310773ebeb20493bcb607c8f8
marthamwangi <martyturing@gmail.com>
2024-06-18 15:48:36 +0300

    chore: export all necessary snippets to be used in the interface all throughout the app

--- libs/store/src/index.ts
+++ libs/store/src/index.ts
@@ -1,3 +1,7 @@
 export * from './lib/lib.routes';
-
-export * from './lib/landing/landing.component';
+export * from './lib/pages/landing/landing.component';
+export * from './lib/data/products.reducers';
+export * from './lib/data/products.effects';
+export * from './lib/data/products.model';
+export * from './lib/pages/product-detail/data/product.effect';
+export * from './lib/pages/product-detail/data/product.reducers';
