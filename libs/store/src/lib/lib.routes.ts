fe67b4154bc4b05eb304d5606532502a5ea7a2c6
marthamwangi <martyturing@gmail.com>
2024-06-18 15:48:51 +0300

    chore: add product routings

--- libs/store/src/lib/lib.routes.ts
+++ libs/store/src/lib/lib.routes.ts
@@ -4,6 +4,15 @@ export const STORE_ROUTES: Route[] = [
   {
     path: '',
     loadComponent: () =>
-      import('./landing/landing.component').then((c) => c.LandingComponent),
+      import('./pages/landing/landing.component').then(
+        (c) => c.LandingComponent
+      ),
+  },
+  {
+    path: ':id',
+    loadComponent: () =>
+      import('./pages/product-detail/product-detail.component').then(
+        (c) => c.ProductDetailComponent
+      ),
   },
 ];
