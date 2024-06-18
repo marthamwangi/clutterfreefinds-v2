d502aa44c809d0c0bf7e8cd376b5a2f6b371eb4b
marthamwangi <martyturing@gmail.com>
2024-06-18 15:52:34 +0300

    chore: show the shop features

---
+++ libs/store/src/lib/pages/landing/components/shop-features/shop-features.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cff-store-shop-features',
+  standalone: true,
+  templateUrl: './shop-features.component.html',
+})
+export class ShopFeaturesComponent {}
