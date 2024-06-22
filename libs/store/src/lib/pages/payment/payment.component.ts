1428c5a17070e12994958cd3786e0afdfbe086d1
marthamwangi <martyturing@gmail.com>
2024-06-22 14:46:52 +0300

    chore: payment component

---
+++ libs/store/src/lib/pages/payment/payment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'cff-store-payment',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payment.component.html',
+})
+export class PaymentComponent {}
