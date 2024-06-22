90257422065906d2ffc4a21bb7ded58824ad56e1
marthamwangi <martyturing@gmail.com>
2024-06-22 14:50:01 +0300

    checkout component

---
+++ libs/store/src/lib/pages/checkout/checkout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'cff-store-checkout',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './checkout.component.html',
+})
+export class CheckoutComponent {}
